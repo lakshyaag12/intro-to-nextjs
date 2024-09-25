2ffc455e1b48e466b7142e1c6d61081a4ec2ba0b
Lakshya Agarwal <lakshya@memorang.com>
2024-09-25 10:58:41 -0400

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | ACME Dashboard",
+    default: "ACME Dashboard",
+  },
+  description: "The official dashboard for ACME",
+  metadataBase: new URL("https://acme-dashboard.vercel.app"),
+};
 
 export default function RootLayout({
   children,
